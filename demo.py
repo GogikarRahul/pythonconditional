81c88cfd5c89fc93dd2c53b1ecdf875a8494b050
GogikarRahul <rahul.gogikar04@gmail.com>
2025-03-26 19:30:03 +0530

    msg

--- demo.py
+++ demo.py
@@ -1,168 +1,169 @@
-num=int(input("enter the number:"))
-if(num>0):
-    print("number is positive")
-elif(num==0):
-    print("number is zero")
-else:
-    print("number is negative")
+# num=int(input("enter the number:"))
+# if(num>0):
+#     print("number is positive")
+# elif(num==0):
+#     print("number is zero")
+# else:
+#     print("number is negative")
 
 
-num=int(input("enter the number:"))
-if(num%2==0):
-    print(num,"is even number")
-else:
-    print(num,"is odd number")
+# num=int(input("enter the number:"))
+# if(num%2==0):
+#     print(num,"is even number")
+# else:
+#     print(num,"is odd number")
 
-age=int(input("enter the number:"))
-if(age>=18):
-    print("hey you can vote")
-else:
-    print("you cannot vote")
-
-
-num1=int(input("enter the first number:"))
-num2=int (input("enter the second number:"))
-if(num1>num2):
-    print( num1,"number is greatest ")
-else:
-    print(num2,"is greater")
-
-marks=int(input("enter the marks:"))
-if(marks>=40):
-    print("pass")
-else:
-    print("fail")
-
-day=int(input("enter the day:"))
-if(day==1):
-    print("monday")
-elif(day==2):
-    print("tuesady")
-elif(day==3):
-    print("wednesday")
-elif(day==4):
-    print("thursday")
-elif(day==5):
-    print("friday")
-elif(day==6):
-    print("saturday")
-elif(day==7):
-    print("sunday")
-elif(day>7):
-    print("enter the correct number which is less than 7")
-else:
-    print("nothing")
+# age=int(input("enter the number:"))
+# if(age>=18):
+#     print("hey you can vote")
+# else:
+#     print("you cannot vote")
 
 
-# calculator
-num1=int(input("enter the number:"))
-operator=input("enter the operator:")
-num2=int(input("enter the second number:"))
+# num1=int(input("enter the first number:"))
+# num2=int (input("enter the second number:"))
+# if(num1>num2):
+#     print( num1,"number is greatest ")
+# else:
+#     print(num2,"is greater")
 
-if(operator== '+'):
-    print(num1+num2)
-elif(operator== '-'):
-    print(num1-num2)
-elif(operator== '/'):
-    print(num1/num2)
-elif(operator=='*'):
-    print(num1*num2)
-elif(operator=='//'):
-    print(num1//num2)
-elif(operator=='%'):
-    print(num1%num2)
-elif(operator != '+','-','/','*' ,'//','%' ):
-    print("enter the valid operator!!")
-else:
-    print(" enter valid operator")
+# marks=int(input("enter the marks:"))
+# if(marks>=40):
+#     print("pass")
+# else:
+#     print("fail")
 
-
-#printing the month
-
-month=int(input("enter the month number:"))
-if(month==1):
-    print("january")
-elif(month==2):
-    print("Febraury")
-elif(month==3):
-    print("March")
-elif(month==4):
-    print("April")
-elif(month==5):
-    print("May")
-elif(month==6):
-    print("june")
-elif(month==7):
-    print("july")
-elif(month==8):
-    print("August")
-elif(month==9):
-    print("September")
-elif(month==10):
-    print("October")
-elif(month==11):
-    print("november")
-elif(month==12):
-    print("December")
-elif(month>12):
-    print("enter the valid month number!!")
-else:
-    print("hey there you go")
+# day=int(input("enter the day:"))
+# if(day==1):
+#     print("monday")
+# elif(day==2):
+#     print("tuesady")
+# elif(day==3):
+#     print("wednesday")
+# elif(day==4):
+#     print("thursday")
+# elif(day==5):
+#     print("friday")
+# elif(day==6):
+#     print("saturday")
+# elif(day==7):
+#     print("sunday")
+# elif(day>7):
+#     print("enter the correct number which is less than 7")
+# else:
+#     print("nothing")
 
 
-num1=int(input("enter the first number:"))
-num2=int(input("enter the second number:"))
-num3=int(input("enter the number three:"))
-if(num1>num2>num3):
-    print(num1," first is greatest")
-elif(num2>num1>num3):
-    print(num2," second is greatest")
-elif(num3>num1>num2):
-    print( num3,"third is greatest")
-else:
-    print("enter the valid number")
+# # calculator
+# num1=int(input("enter the number:"))
+# operator=input("enter the operator:")
+# num2=int(input("enter the second number:"))
 
-year=int(input("enter the year:"))
-if(year%400==0):
-    print(year,"it is a leap year")
-elif(year%4 ==0 and year% 100 !=0):
-    print(year,"it is  a leap year")
-else:
-    print("It is not a leap year")
+# if(operator== '+'):
+#     print(num1+num2)
+# elif(operator== '-'):
+#     print(num1-num2)
+# elif(operator== '/'):
+#     print(num1/num2)
+# elif(operator=='*'):
+#     print(num1*num2)
+# elif(operator=='//'):
+#     print(num1//num2)
+# elif(operator=='%'):
+#     print(num1%num2)
+# elif(operator != '+','-','/','*' ,'//','%' ):
+#     print("enter the valid operator!!")
+# else:
+#     print(" enter valid operator")
 
-char=input("Enter the character:")
-if(char=='a'):
-    print(char,"it is a vowel")
-elif(char=='A'):
-    print(char,"it is a vowel")
-elif(char=='e'):
-    print(char,"it is a vowel")
-elif(char=='E'):
-    print(char,"it is a vowel")
-elif(char=='i'):
-    print(char,"it is a vowel")
-elif(char=='I'):
-    print(char,"it is a vowel")
+
+# #printing the month
+
+# month=int(input("enter the month number:"))
+# if(month==1):
+#     print("january")
+# elif(month==2):
+#     print("Febraury")
+# elif(month==3):
+#     print("March")
+# elif(month==4):
+#     print("April")
+# elif(month==5):
+#     print("May")
+# elif(month==6):
+#     print("june")
+# elif(month==7):
+#     print("july")
+# elif(month==8):
+#     print("August")
+# elif(month==9):
+#     print("September")
+# elif(month==10):
+#     print("October")
+# elif(month==11):
+#     print("november")
+# elif(month==12):
+#     print("December")
+# elif(month>12):
+#     print("enter the valid month number!!")
+# else:
+#     print("hey there you go")
+
+
+# num1=int(input("enter the first number:"))
+# num2=int(input("enter the second number:"))
+# num3=int(input("enter the number three:"))
+# if(num1>=num2 and num1>=num3):
+#     print(num1," first is greatest")
+# elif(num2>=num1 and num2>=num3):
+#     print(num2," second is greatest")
+# elif(num3>=num1 and num3>=num2):
+#     print( num3,"third is greatest")
+# else:
+#     print("enter the valid number")
+
+# year=int(input("enter the year:"))
+# if(year%400==0):
+#     print(year,"it is a leap year")
+# elif(year%4 ==0 and year% 100 !=0):
+#     print(year,"it is  a leap year")
+# else:
+#     print("It is not a leap year")
+
+# char=input("Enter the character:")
+# if(char=='a'):
+#     print(char,"it is a vowel")
+# elif(char=='A'):
+#     print(char,"it is a vowel")
+# elif(char=='e'):
+#     print(char,"it is a vowel")
+# elif(char=='E'):
+#     print(char,"it is a vowel")
+# elif(char=='i'):
+#     print(char,"it is a vowel")
+# elif(char=='I'):
+#     print(char,"it is a vowel")
    
-elif(char=='o'):
-    print(char,"it is a vowel")
+# elif(char=='o'):
+#     print(char,"it is a vowel")
    
-elif(char=='O'):
-   print(char,"it is a vowel")
+# elif(char=='O'):
+#    print(char,"it is a vowel")
    
-elif(char != 'a','A','e','E','i','I','o','O','e','E'):
-    print(char,"it is  a consonent")
-else:
-    print("enter the correct character")
+# elif(char != 'a','A','e','E','i','I','o','O','e','E'):
+#     print(char,"it is  a consonent")
+# else:
+#     print("enter the correct character")
    
-grade=int(input("Enter the number:"))
-if(grade>=90 and grade<=100):
-    print("Grade A")
-elif(grade>=80 and grade<90):
-    print("Grade B")
-elif(grade>=70 and grade<80):
-    print("Grade C")
-else:
-    print("Fail")
+# grade=int(input("Enter the number:"))
+# if(grade>=90 and grade<=100):
+#     print("Grade A")
+# elif(grade>=80 and grade<90):
+#     print("Grade B")
+# elif(grade>=70 and grade<80):
+#     print("Grade C")
+# else:
+#     print("Fail")
 
-   
+# fizz=int(input("enter the number:"))
+# if(fizz%15==0):
---
+++ pythonloops/hello.py
@@ -0,0 +1,90 @@
+# for i in range(1,101):
+#     print(i)
+
+#******************************************************
+# sum=0
+# num=int(input("enter the number"))
+# for i in range(1,num+1):
+#      sum+=i
+# print(sum)
+#**********************************************************
+# num=0
+# while num<50:
+#     num+=1
+#     if(num%2==0):
+#         print(num)
+
+#********************************************
+
+# num=int(input("enter the number:"))
+# for i in range(1,11):
+#     print(num*i)
+
+#*******************************************************
+
+# num=int(input("enter the number:"))
+# sum=0
+# while num+1>0:
+#     sum+=num
+#     print(num)
+#     num-=1
+# print(sum)
+
+#***********************************************************
+# num=(input("enter the number:"))
+# while num>0:
+#     print(str(len(num)))
+#     break
+
+
+# for i in range(1,11):
+#     if(i%2==0):
+#         continue
+#     for j in range(1,31):
+#          print(i,j)
+
+
+# for i in range(1,11):
+#       for j in range(1,31):
+#           if(i%2!=0 and j<15):
+#               continue
+#           print(i,j)
+          
+# i=3
+# print("the number is even")if(i%2==0)else print("the number is odd")
+
+
+# char=input("enter the single character").lower()
+# if len(char)==1:
+#     if char.isalpha():
+#         if char in ['a','e','i','o','u']:
+#             print('is is a vowel')
+#         else:
+#             print('it is a consonant')
+#     else:
+#         print('it is a special character')
+# else:
+#     print('please Enter a valid character greater than 1')
+        
+#*************************************************************************************************
+
+# num1=13691215222325
+# temp=0
+# sum=0
+# count=0
+
+# while num1>0:
+#     rem=num1%10 
+#     sum+=rem
+#     if(rem%3==0):
+#      print(rem)
+#     temp=temp*10+rem
+#     num1=num1//10
+#     count+=1
+   
+      
+# print(temp)
+# print(sum)
+# print("count=",count)
+
+
